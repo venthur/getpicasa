06cf0200521c1625a7fb7f714972a39aa1927e96
Bastian Venthur <venthur@debian.org>
2010-09-27 10:45:44 +0200

    Added bsd license.

--- getpicasa.py
+++ getpicasa.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2010, Bastian Venthur
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+"""Download picasa webalbums given a URL."""
+
 from xml.dom import minidom
 import urllib2
 import sys
@@ -26,5 +40,7 @@ def main(rssurl):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: %s rss-url" % sys.argv[0]
     main(sys.argv[1])
 
